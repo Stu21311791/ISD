9467f66d7ee6a5bab6f1b36e32e775b2c26c61e3
Stu21311791 <21311791@student.uwl.ac.uk>
2016-12-15 12:02:38 +0000

    Add files via upload

---
+++ cash.py
@@ -0,0 +1,23 @@
+class Cashregister():
+
+ Def__init__(sel,itemCount=0,totalPrice=0.0):
+  self._itemCount=itemCount
+  self._totalPrice=totalPrice
+
+ Def addItem(self,Price):
+  self._itemCount=self._itemCount+1
+  self._totalPrice=self._totalPrice+price
+
+ Def getTotal(self):
+  return self._totalPrice
+
+
+ Def getTotal(self):
+  return self._totalCount
+
+
+ Def clear(self):
+  self._itemCount=0
+  self._totalPrice=0.0
+     
+    
